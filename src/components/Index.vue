eea144fba5f6c7e7ed0dca4ef788cefc7f719dbc
Eatherly <eatherly45@gmail.com>
2020-11-23 11:02:52 +0200

    eslint fix .2

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,13 +1,13 @@
 <template>
   <section id="main">
-    
+
     <div class="container">
       <b-navbar toggleable="lg" type="dark" variant="info">
-          <b-navbar-brand href="#">BUDIES LIST</b-navbar-brand>
+        <b-navbar-brand href="#">BUDIES LIST</b-navbar-brand>
       </b-navbar>
 
       <b-container class="bv-example-row">
-          <b-row  class="text-center header">
+        <b-row class="text-center header">
           <b-col cols="1">#</b-col>
           <b-col class="text-left" cols="3">First Name</b-col>
           <b-col class="text-left" cols="3">Last Name</b-col>
@@ -17,20 +17,25 @@
         </b-row>
 
         <b-row v-for="(budy, i) in budies" :key="budy.email" class="text-center items-of-list">
-          <b-col cols="1">{{i+1}}</b-col>
-          <b-col class="text-left" cols="3">{{budy.name}}</b-col>
-          <b-col class="text-left" cols="3">{{budy.lastName}}</b-col>
-          <b-col class="text-left" cols="3">{{budy.email}}</b-col>
-          <b-col @click="clickEdit(budy)" class="edit" cols="1"><b-icon icon="pencil-fill"></b-icon></b-col>
-          <b-col @click="clickDelete(budy)" class="delete" cols="1"><b-icon icon="trash-fill" aria-hidden="true"></b-icon></b-col>
+          <b-col cols="1">{{ i + 1 }}</b-col>
+          <b-col class="text-left" cols="3">{{ budy.name }}</b-col>
+          <b-col class="text-left" cols="3">{{ budy.lastName }}</b-col>
+          <b-col class="text-left" cols="3">{{ budy.email }}</b-col>
+          <b-col @click="clickEdit(budy)" class="edit" cols="1">
+            <b-icon icon="pencil-fill"></b-icon>
+          </b-col>
+          <b-col @click="clickDelete(budy)" class="delete" cols="1">
+            <b-icon icon="trash-fill" aria-hidden="true"></b-icon>
+          </b-col>
         </b-row>
 
       </b-container>
-      <b-row  class="text-right add-budy-bar">
-          <b-col cols="12">
-            <b-button v-show="false" @click="clickDelete">Simple msgBoxConfirm</b-button>
-            <userForm  :budyForEdit="budyForEdit" :budies="budies" :editCase="editCase" @reset-edit-mode="resetEditMode"  @add-edit-budy="addEditBudy"></userForm>
-          </b-col>
+      <b-row class="text-right add-budy-bar">
+        <b-col cols="12">
+          <b-button v-show="false" @click="clickDelete">Simple msgBoxConfirm</b-button>
+          <userForm :budyForEdit="budyForEdit" :budies="budies" :editCase="editCase" @reset-edit-mode="resetEditMode"
+                    @add-edit-budy="addEditBudy"></userForm>
+        </b-col>
       </b-row>
     </div>
   </section>
@@ -38,93 +43,95 @@
 
 <script>
 
-  import  userForm  from "./modals/user-form.vue";
+import userForm from "./modals/user-form.vue";
 
-  export default {
-    components: {userForm},
-    data() {
-      return {
-        deleteModal: false,
-        editModal: false,
-        editCase: 0,
-        budies: [ ],
-        budyForEdit: {}
-      };
+export default {
+  components: {userForm},
+  data() {
+    return {
+      deleteModal: false,
+      editModal: false,
+      editCase: 0,
+      budies: [],
+      budyForEdit: {}
+    };
+  },
+  created() {
+    this.budies = JSON.parse(localStorage.getItem("budies")) || [];
+  },
+  methods: {
+
+    clickDelete(budy) {
+      this.$bvModal.msgBoxConfirm("Are you sure?")
+        .then(value => {
+          if (value) {
+            this.budies = this.budies.filter(item => item.email != budy.email);
+            localStorage.setItem("budies", JSON.stringify(this.budies));
+          }
+        });
     },
-    created() {
-      this.budies =  JSON.parse(localStorage.getItem("budies")) || [];
+
+    resetEditMode() {
+      this.budyForEdit = {};
     },
-    methods: {
 
-      clickDelete(budy) {
-        this.$bvModal.msgBoxConfirm("Are you sure?")
-          .then(value => {
-              if(value){
-                this.budies = this.budies.filter(item => item.email != budy.email);
-                localStorage.setItem("budies", JSON.stringify(this.budies));
-                }
-          });
-      }, 
+    addEditBudy(budy) {
+      if (this.budyForEdit.email != budy.email) {
+        this.budies.push(budy);
+      } else this.budies = this.budies.map(item => item.email == budy.email ? budy : item);
+      this.resetEditMode();
+      localStorage.setItem("budies", JSON.stringify(this.budies));
+    },
 
-      resetEditMode () {
-        this.budyForEdit = {};
-      },
-
-      addEditBudy (budy) {
-        if(this.budyForEdit.email != budy.email ) {
-          this.budies.push(budy);
-        }
-        else this.budies = this.budies.map(item => item.email == budy.email ? budy : item);
-        this.resetEditMode();
-        localStorage.setItem("budies", JSON.stringify(this.budies));
-      },
-
-      clickEdit (budy) {
-        this.budyForEdit = budy;
-        this.editCase++;
-      }
-      
+    clickEdit(budy) {
+      this.budyForEdit = budy;
+      this.editCase++;
     }
-  };
+
+  }
+};
 </script>
-<style lang="scss" scoped >
+<style lang="scss" scoped>
 
-  .header {
-    font-weight: bold;
-    height: 2.5rem;
-    line-height: 2.5rem;
-    background-color: rgba(86,61,124,.15);
-    div:not(:last-child) {
-      border-right: 1px solid grey ;
-    }
+.header {
+  font-weight: bold;
+  height: 2.5rem;
+  line-height: 2.5rem;
+  background-color: rgba(86, 61, 124, .15);
+
+  div:not(:last-child) {
+    border-right: 1px solid grey;
+  }
+}
+
+.bv-example-row {
+  border: 1px solid grey;
+}
+
+.items-of-list {
+  border-top: 1px solid grey;
+  line-height: 2rem;
+  height: 2rem;
+
+  &:nth-child(odd) {
+    background-color: rgba(255, 0, 0, .1);
+
   }
 
-  .bv-example-row {
-    border: 1px solid grey ;
+  div:not(:last-child) {
+    border-right: 1px solid grey;
   }
 
-  .items-of-list {
-    border-top: 1px solid grey ;
-    line-height: 2rem;
-    height: 2rem;
-    &:nth-child(odd) {
-      background-color: rgba(255,0,0,.1);
-      
-    }
-    div:not(:last-child) {
-      border-right: 1px solid grey ;
-    }
-    
-  }
+}
 
-  .add-budy-bar {
-    margin-top: 20px
-  }
+.add-budy-bar {
+  margin-top: 20px
+}
 
 
-  .edit, .delete {
-    cursor: pointer;
-  }
+.edit, .delete {
+  cursor: pointer;
+}
 
 </style>
 
--- src/components/modals/user-form.vue
+++ src/components/modals/user-form.vue
@@ -2,54 +2,54 @@
   <div>
     <b-button ref="addEditBtn" v-b-modal.modal-prevent-closing variant="primary">Add budy</b-button>
     <b-modal
-      id="modal-prevent-closing"
-      ref="modal"
-      title="User Card"
-      @show="resetModal"
-      @hidden="hiddenModal"
-      @ok="handleOk"
+        id="modal-prevent-closing"
+        ref="modal"
+        title="User Card"
+        @show="resetModal"
+        @hidden="hiddenModal"
+        @ok="handleOk"
     >
       <form ref="form" @submit.stop.prevent="handleSubmit">
         <b-form-group
-          :state="nameState"
-          label="Name"
-          label-for="name-input"
-          invalid-feedback="Name is required & should't be short"
-        >
-          <b-form-input
-            id="name-input"
-            v-model.trim="name"
             :state="nameState"
-            required
-          ></b-form-input>
-        </b-form-group>
-        
-        <b-form-group
-          :state="lastNameState"
-          label="Last name"
-          label-for="last-name-input"
-          invalid-feedback="Last name is required "
+            label="Name"
+            label-for="name-input"
+            invalid-feedback="Name is required & should't be short"
         >
           <b-form-input
-            id="last-name-input"
-            v-model="lastName"
+              id="name-input"
+              v-model.trim="name"
+              :state="nameState"
+              required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
             :state="lastNameState"
-            required
-          ></b-form-input>
-        </b-form-group>
-        
-        <b-form-group
-          :state="emailState"
-          label="Email"
-          label-for="email-input"
-          invalid-feedback="Email is not correct"
+            label="Last name"
+            label-for="last-name-input"
+            invalid-feedback="Last name is required "
         >
           <b-form-input
-            id="email-input"
-            v-model="email"
+              id="last-name-input"
+              v-model="lastName"
+              :state="lastNameState"
+              required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
             :state="emailState"
-            :disabled="!!budyForEdit.email"
-            required
+            label="Email"
+            label-for="email-input"
+            invalid-feedback="Email is not correct"
+        >
+          <b-form-input
+              id="email-input"
+              v-model="email"
+              :state="emailState"
+              :disabled="!!budyForEdit.email"
+              required
           ></b-form-input>
           <small class="warning_exist" v-if="alreadyExist">This email already exist</small>
         </b-form-group>
@@ -59,127 +59,125 @@
 </template>
 
 <script>
-  import {email, required, minLength} from "vuelidate/lib/validators";
- 
+import {email, required, minLength} from "vuelidate/lib/validators";
 
-  export default {
-    props: {
-      budies: {
-        type: Array
-      },
-      budyForEdit : {
-        type: Object,
-        default: () => ({
-            name: "",
-            lastName: "",
-            email: ""
-          }
-        )
-      },
-      editCase: {
-        type: Number,
-        default: 0
-      }
+
+export default {
+  props: {
+    budies: {
+      type: Array
     },
-    data() {
-      return {
+    budyForEdit: {
+      type: Object,
+      default: () => ({
         name: "",
         lastName: "",
-        email: "",
-        alreadyExist: false,
-        budy: {},
-        editMode: false
-      };
+        email: ""
+      })
     },
-    validations: {
-        name: {required, minLength: minLength(3)},
-        lastName: {required, minLength: minLength(3)},
-        email: {required, email }
-    },
-
-    computed: {
-      nameState () {
-        return this.name.length ? this.$v.name.required && this.$v.name.minLength : null;
-      },
-      lastNameState () {
-        return this.lastName.length ? this.$v.lastName.required && this.$v.lastName.minLength : null;
-      },
-      emailState () {
-        return this.email.length ? this.$v.email.required && this.$v.email.email : null;
-      }
-    },
-
-    mounted () {
-      this.$watch("editCase",
-        function () {
-          this.name = this.budyForEdit.name;
-          this.lastName = this.budyForEdit.lastName;
-          this.email = this.budyForEdit.email;
-          this.$refs.addEditBtn.click();
-        }
-      );
-    },
-
-    methods: {
-      checkEmail() {
-         if (!this.budyForEdit.email && this.budies.find((item) => item.email == this.email)) {
-           this.alreadyExist = true;
-           return true;
-         }
-         else this.alreadyExist = false;
-         return false;
-      },
-
-      resetModal() {    
-        this.alreadyExist = false;
-        this.nameState = null;
-        this.lastNameState = null;
-        this.emailState = null;
-        if(this.budyForEdit.email) return;
-        this.name = "";
-        this.lastName = "";
-        this.email = "";
-      },
-
-      hiddenModal () {
-        this.$emit("reset-edit-mode");
-        this.resetModal();
-      },
-
-      handleOk(bvModalEvt) {
-        // Prevent modal from closing
-        bvModalEvt.preventDefault();
-        // Trigger submit handler
-        this.handleSubmit();
-      },
-
-      handleSubmit() {
-        if(this.$v.$invalid) {
-           return;
-         }
-
-         if (this.checkEmail() && !this.budyForEdit.email) {
-          return;
-        }
-        
-        this.budy.name = this.name;
-        this.budy.lastName = this.lastName;
-        this.budy.email = this.email;
-
-
-        this.$emit("add-edit-budy", this.budy);
-        this.budy = {};
-        //this.$emit('reset-edit-mode');
-        this.resetModal();
-        this.$nextTick(() => {
-          this.$bvModal.hide("modal-prevent-closing");
-        });
-      }
+    editCase: {
+      type: Number,
+      default: 0
     }
-  };
-</script>
-<style lang="scss" scoped >
-  .warning_exist {
-    color: red;
+  },
+  data() {
+    return {
+      name: "",
+      lastName: "",
+      email: "",
+      alreadyExist: false,
+      budy: {},
+      editMode: false
+    };
+  },
+  validations: {
+    name: {required, minLength: minLength(3)},
+    lastName: {required, minLength: minLength(3)},
+    email: {required, email}
+  },
+
+  computed: {
+    nameState() {
+      return this.name.length ? this.$v.name.required && this.$v.name.minLength : null;
+    },
+    lastNameState() {
+      return this.lastName.length ? this.$v.lastName.required && this.$v.lastName.minLength : null;
+    },
+    emailState() {
+      return this.email.length ? this.$v.email.required && this.$v.email.email : null;
+    }
+  },
+
+  mounted() {
+    this.$watch("editCase",
+      function () {
+        this.name = this.budyForEdit.name;
+        this.lastName = this.budyForEdit.lastName;
+        this.email = this.budyForEdit.email;
+        this.$refs.addEditBtn.click();
+      }
+    );
+  },
+
+  methods: {
+    checkEmail() {
+      if (!this.budyForEdit.email && this.budies.find((item) => item.email == this.email)) {
+        this.alreadyExist = true;
+        return true;
+      } else this.alreadyExist = false;
+      return false;
+    },
+
+    resetModal() {
+      this.alreadyExist = false;
+      this.nameState = null;
+      this.lastNameState = null;
+      this.emailState = null;
+      if (this.budyForEdit.email) return;
+      this.name = "";
+      this.lastName = "";
+      this.email = "";
+    },
+
+    hiddenModal() {
+      this.$emit("reset-edit-mode");
+      this.resetModal();
+    },
+
+    handleOk(bvModalEvt) {
+      // Prevent modal from closing
+      bvModalEvt.preventDefault();
+      // Trigger submit handler
+      this.handleSubmit();
+    },
+
+    handleSubmit() {
+      if (this.$v.$invalid) {
+        return;
+      }
+
+      if (this.checkEmail() && !this.budyForEdit.email) {
+        return;
+      }
+
+      this.budy.name = this.name;
+      this.budy.lastName = this.lastName;
+      this.budy.email = this.email;
+
+
+      this.$emit("add-edit-budy", this.budy);
+      this.budy = {};
+      //this.$emit('reset-edit-mode');
+      this.resetModal();
+      this.$nextTick(() => {
+        this.$bvModal.hide("modal-prevent-closing");
+      });
+    }
   }
+};
+</script>
+<style lang="scss" scoped>
+.warning_exist {
+  color: red;
+}
 </style>
